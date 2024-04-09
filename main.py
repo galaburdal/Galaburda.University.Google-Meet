72bc7cee06ab036683eb74d4b8db9c03d3e90cc6
galaburdal <zpipzzzzpo1@gmail.com>
2024-04-09 20:17:44 +0300

    new tasks

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-# main.py
-
-from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SECRET_KEY'] = 'i_love_very_my_dog'
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@socketio.on('register')
-def handle_register(data):
-    # Handle user registration logic here
-    username = data['username']
-    email = data['email']
-    # Save user data to database or perform necessary actions
-    emit('registration-success', {'message': 'Registration successful'}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
